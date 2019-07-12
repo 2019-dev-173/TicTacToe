d1cb0618881366e1a38cd3fa91e620c9452c4d83
2019_dev_173 <51744722+2019-dev-173@users.noreply.github.com>
2019-07-12 15:13:55 +0200

    Commit Function Game CurrentPlayer and put

--- src/Game.js
+++ src/Game.js
@@ -1,1 +1,17 @@
+function Game() {
+  this.current_player = "first"
+  this.fields = { first: [], second: [] }
+}
 
+Game.prototype.currentPlayer = function() {
+  return this.current_player
+}
+
+Game.prototype.put = function(player, field) {
+  this.fields[player].push(field)
+  if (this.currentPlayer() == "first") {
+    this.current_player = "second"
+  } else {
+    this.current_player = "first"
+  }
+}
